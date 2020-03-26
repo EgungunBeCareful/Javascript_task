3cd57ce66b3f591d2ef3cea877b01cbc29289612
EgungunBeCareful <33954352+EgungunBeCareful@users.noreply.github.com>
2020-03-26 11:06:01 +0100

    Add files via upload

---
+++ JS Task1.js
@@ -0,0 +1,16 @@
+//Store your name in a varibale. 
+let name = "ELUWA CHISOM";
+
+//Store your courses in an array.
+let courses = ["HTML", "CSS","PYTHON","JAVASCRIPT", "KOTLIN"];
+
+//Display your name and courses and do one of the following: 
+
+console.log(name);
+console.log(courses);
+
+//If the number of courses you are doing is an odd number, 
+//find and display all odd numbers from 1-200 (inclusive)
+for (let i=1; i<=200; i++)
+    if (i % 2 !==0 )
+    console.log(i)
